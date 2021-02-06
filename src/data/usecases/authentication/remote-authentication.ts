be43bec6ea57318424180ba2ff90c6488733177e
wallacesilvaferreira <wallace.ferreira@fdte.io>
2021-02-06 18:37:10 -0300

    feat: ensure RemoteAuthentication

---
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -0,0 +1,15 @@
+import { HttpPostClient } from 'data/protocols/http/http-post-client'
+
+export class RemoteAuthentication {
+  private readonly url: string
+  private readonly httpPostClient: HttpPostClient
+
+  constructor (url: string, httpPostClient: HttpPostClient) {
+    this.url = url
+    this.httpPostClient = httpPostClient
+  }
+
+  async auth (): Promise<void> {
+    await this.httpPostClient.post(this.url)
+  }
+}
